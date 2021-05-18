180e544053f75abb700faae16379be5536a58368
Anderson Almeida <anderson.almeida@radocc.com>
2021-05-18 18:16:16 -0300

    update

--- lib/app/repositories/conteudo_visualizado_repository.dart
+++ lib/app/repositories/conteudo_visualizado_repository.dart
@@ -1,12 +1,30 @@
+import 'dart:convert';
 import 'package:blink/app/database/database.dart';
-import 'package:blink/app/models/enuns/dados_dentificacao_enum.dart';
+import 'package:blink/app/repositories/abstract_repository.dart';
 
 import 'dados_repository.dart';
 
-class ConteudoVisualizadoRepository extends DadosRepository<Conteudo> {
+class ConteudoVisualizadoRepository extends AbstractRepository {
   // final Dio dio;
 
-  ConteudoVisualizadoRepository() : super(DadosIdentificacao.CONTEUDO_VISUALIZADO);
+  ConteudoVisualizadoRepository() : super("conteudovisualizado");
+
+
+  Future<List<ConteudoVisualizado>> enviar(List<ConteudoVisualizado> lista) async {
+    try {
+      var resp = await dio.post(getUrl('/salvar/visualizados'), data:lista);
+      var vetor = jsonDecode(resp.data);
+      var listaRetornada = [];
+      vetor.forEach((cv) {
+        listaRetornada.add(ConteudoVisualizado.fromJson(cv));
+      });
+      return listaRetornada;
+    } catch (error) {
+      //print(error.response.statusCode);
+      throw Exception("Exception occured: $error");
+    }
+  }
+
 }
 
  
