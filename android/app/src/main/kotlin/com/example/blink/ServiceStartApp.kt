78176e1300c87019714d56693d9fd621e8a0add5
brenomenosso <breno_menosso@hotmail.com>
2021-06-02 18:00:06 -0300

    Ajuste auto start app blink

--- android/app/src/main/kotlin/com/example/blink/ServiceStartApp.kt
+++ android/app/src/main/kotlin/com/example/blink/ServiceStartApp.kt
@@ -1,13 +1,14 @@
-package com.example.dfssdfsdfsdf
+package com.example.blink
 
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import com.example.blink.MainActivity
 
 class ServiceStartApp : BroadcastReceiver() {
+    val ACTION = "android.intent.action.BOOT_COMPLETED"
+
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action.equals("android.intent.action.BOOT_COMPLETED")) {
+        if (intent.action.equals(ACTION)) {
             val i = Intent(context, MainActivity::class.java)
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             context.startActivity(i)
