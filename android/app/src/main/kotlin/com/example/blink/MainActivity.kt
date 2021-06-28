2f7ae6deb4fb141c6912b1a5d89d6969579829ef
brenomenosso <breno_menosso@hotmail.com>
2021-06-28 13:25:48 -0300

    Atualizações

--- android/app/src/main/kotlin/com/example/blink/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/blink/MainActivity.kt
@@ -3,27 +3,26 @@ package com.example.blink
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.provider.Settings
-import androidx.annotation.NonNull;
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
-public class MainActivity: FlutterActivity() {
+class MainActivity: FlutterActivity() {
 
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        //super.configureFlutterEngine(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            if (!Settings.canDrawOverlays(getApplicationContext())) {
+            if (!Settings.canDrawOverlays(applicationContext)) {
                 startActivity(Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION))
             }
         }
-
     }
 }
--- android/app/src/main/kotlin/com/example/blink/ServiceStartApp.kt
+++ android/app/src/main/kotlin/com/example/blink/ServiceStartApp.kt
@@ -4,14 +4,11 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
-class ServiceStartApp : BroadcastReceiver() {
-    val ACTION = "android.intent.action.BOOT_COMPLETED"
+class ServiceStartApp : BroadcastReceiver() {   
 
     override fun onReceive(context: Context, intent: Intent) {
-        if (intent.action.equals(ACTION)) {
-            val i = Intent(context, MainActivity::class.java)
-            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context.startActivity(i)
-        }
+        val i = Intent(context, MainActivity::class.java)
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        context.startActivity(i)
     }
 }
